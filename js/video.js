04e5130ab8ae3d86057e57ebee2a1d3b4eedfac0
zns <liu1131451057@gmail.com>
2020-09-26 00:21:47 +0800

    Site updated: 2020-09-26 00:21:41

--- js/video.js
+++ js/video.js
@@ -1,7 +1,7 @@
 const dp = new DPlayer({
     container: document.getElementById('dplayer'),
-    video: {
-        url: 'https://stszjl.aoscdn.com/app/showmore/transcoding/video/afe19eaeeccc8e18c718f4a62a3acd71/lsghtve.mp4?auth_key=1601018759-793877-416038-c904409f68ac73e018213ebecd27d32d&Expires=1601018759',
+    video: {    
+        url: 'https://stszjl.aoscdn.com/app/showmore/transcoding/video/afe19eaeeccc8e18c718f4a62a3acd71/lsghtve.mp4?auth_key=1601054398-649608-518842-078dae50d65978ed219c1bb5738cce4b&Expires=1601054398',
         pic: 'https://storage.live.com/items/2EBEB8465D8C4C1E!2521?authkey=%21AOCrgXvkbYgCLpk',
         type: 'auto',
     },
@@ -11,4 +11,5 @@ const dp = new DPlayer({
         },
     },
 });
+
 console.log(dp.plugins.hls); // Hls 实例
